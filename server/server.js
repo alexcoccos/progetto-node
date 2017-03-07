f3e8e4a9011dab6522bf57db55aab4bb93a999c1
Alex Contursi <contursi.axel@gmail.com>
2017-03-07 15:26:38 +0100

    creato index

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 const PORT = 3000;
 
 app.listen(PORT,function(){
   console.log('server start at http://localhost:'+PORT);
+});
+
+
+//servo la index.html
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname,"..","client","index.html"));
 });
