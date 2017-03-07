8345a379d00cecb2fe0ecd2194004c47c669a70c
Alex Contursi <contursi.axel@gmail.com>
2017-03-07 15:37:50 +0100

    collegamento a utenti.js

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ var path = require('path');
 
 const PORT = 3000;
 
-app.listen(PORT,function(){
-  console.log('server start at http://localhost:'+PORT);
-});
-
-
 //servo la index.html
-
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,"..","client","index.html"));
+});
+
+var utenti = require('./utenti/utenti.js');
+//per tutte le rotte degli utenti vai al file utenti.js
+app.use('/utenti',utenti);
+
+//server in ascolto sulla porta 3000
+app.listen(PORT,function(){
+  console.log('server start at http://localhost:'+PORT);
 });
